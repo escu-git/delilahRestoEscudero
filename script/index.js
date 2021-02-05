0238bc3dda0eb145695ef7c90a43764e86b6d893
escu-git <escuderopablo@hotmail.com.ar>
2021-02-04 21:43:52 -0300

    Swagger file

--- script/index.js
+++ script/index.js
@@ -2,3 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.listen(port,()=>{
+    console.log(`El server está escuchando el puerto ${port}`)
+})
+
+
+app.get('/', function(req, res){
+    res.send('hello world!')
+})
+
+app.get('/login', function(req, res){
+    res.send ('Ingrese datos para loguear')
+})
+
+app.post('/login', function(req, res){
+    res.render('hola')
+})
